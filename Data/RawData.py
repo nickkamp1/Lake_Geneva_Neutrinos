ab1fbf781a17b4434792105cd3bba5cfc4b98443
nickkamp1 <nwkamp@mit.edu>
2023-11-06 20:29:19 -0500

    First Commit

---
+++ Data/RawData.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append('../')
+from GeometryHelpers import *
+
+LHC_surface_locations = {
+    'ATLAS':(46.23498986744134, 6.053437868403428,440.9),
+    'Point2':(46.25152943082325, 6.0216164390182705,448.0),
+    'Point3':(46.27753,6.01218,536.4),
+    'Point4':(46.30443121543384, 6.036295278821727,585.9),
+    'CMS':(46.30989989177777, 6.077425154913358,510.0),
+    'Point6':(46.293459730167676, 6.111853119979701,471.0),
+    'Point7':(46.266426109460426, 6.114850708334322,433.2),
+    'LHCb':(46.24176461415312, 6.097010072101683,426.6)
+}
+
+LHC_horizontal = {
+    'ATLAS':None,
+    'Point2':None,
+    'Point3':None,
+    'Point4':1696.38,
+    'CMS':None,
+    'Point6':None,
+    'Point7':None,
+    'LHCb':10284.3
+}
+
+LHC_elevation = {
+    'ATLAS':None,
+    'Point2':0.5*(509.82 + 337.02),
+    'Point3':None,
+    'Point4':509.82,
+    'CMS':None,
+    'Point6':0.5*(509.82 + 337.02),
+    'Point7':None,
+    'LHCb':337.02
+}
+
+Lake_edge = [
+    (46.22695,6.14891,379.9),
+    (46.22375,6.15149,376.1),
+    (46.22123,6.15357,376.0),
+    (46.21761,6.15177,387.1),
+    (46.21140,6.15327,373.7),
+    (46.20727,6.16019,372.2),
+    (46.21489,6.16826,370.0),
+    (46.21517,6.17538,375.3),
+    (46.23806,6.19279,371.1),
+    (46.24566,6.19193,372.3),
+    (46.26287,6.19742,374.3),
+    (46.26596,6.20978,370.9),
+    (46.35839,6.29076,378.3),
+    (46.37140,6.32652,376.4),
+    (46.36746,6.35067,374.6),
+    (46.40529,6.87641,370.0),
+    (46.44523,6.87597,373.7),
+    (46.51899,6.58895,378.1),
+    (46.47361,6.43789,392.0),
+    (46.39732,6.28256,375.1),
+    (46.39046,6.27415,376.6),
+    (46.39294,6.25887,375.8),
+    (46.35608,6.21475,374.0),
+    (46.32567,6.20031,377.6),
+    (46.30196,6.17971,375.2),
+    (46.29346,6.17015,376.3),
+    (46.27433,6.17087,378.9),
+    (46.26436,6.16709,375.0),
+    (46.25510,6.15576,377.3)
+]
+
+def main():
+    LHC_locations = {k:(v[0],v[1],LHC_elevation[k]) for k,v in LHC_surface_locations.items()}
+    LHC_locations_cartesian = {k:lat_long_to_xyz(*LHC_locations[k]) for k in ['Point2','Point4','Point6','LHCb']}
+
+
+    LHC_dataframe = pd.DataFrame(index=LHC_surface_locations.keys(),
+                                columns=['Latitude','Longitude','Elevation','SurfaceElevation','X','Y','Z'])
+
+    for k in LHC_surface_locations.keys():
+        LHC_dataframe['Latitude'][k] = LHC_locations[k][0]
+        LHC_dataframe['Longitude'][k] = LHC_locations[k][1]
+        LHC_dataframe['Elevation'][k] = LHC_locations[k][2]
+        LHC_dataframe['SurfaceElevation'][k] = LHC_surface_locations[k][2]
+    for k in LHC_locations_cartesian.keys():
+        LHC_dataframe['X'][k] = LHC_locations_cartesian[k][0]
+        LHC_dataframe['Y'][k] = LHC_locations_cartesian[k][1]
+        LHC_dataframe['Z'][k] = LHC_locations_cartesian[k][2]
+
+    for i in range(len(Lake_edge)): 
+        Lake_edge[i] = (Lake_edge[i][0],Lake_edge[i][1],Lake_geneva_elevation)
+
+    Lake_edge_cartesian = np.array([lat_long_to_xyz(*x) for x in Lake_edge])
+
+    Lake_dataframe = pd.DataFrame(columns=['Latitude','Longitude','Elevation','X','Y','Z'])
+
+    Lake_dataframe[['Latitude','Longitude','Elevation']] = Lake_edge
+    Lake_dataframe[['X','Y','Z']] = Lake_edge_cartesian
+
+    LHC_dataframe.to_parquet('../Data/LHC_data.parquet')
+    Lake_dataframe.to_parquet('../Data/Lake_data.parquet')
---
+++ GeometryHelpers.py
@@ -0,0 +1,147 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation
+import shapely.geometry
+
+R_earth = 6371000.
+Lake_geneva_elevation = 372.
+
+class Circle:
+
+    def __init__(self,x1,x2,x3): 
+        # x1, x2, and x3 are three points on circle
+        # define a 3D circle as the intersection of a sphere and a plane
+        # plane eq: ax + by + cz + d = 0
+        # sphere eq: (x-x0)^2 + (y-y0)^2 + (z-z0)^2 = r^2
+        self.v1 = x1 - x2
+        self.v2 = x1 - x3
+        self.n = np.cross(self.v1,self.v2) # n = (a,b,c)
+        self.d = - np.dot(self.n,x1)
+        self.center = find_circle_center(x1,x2,x3)
+        self.radius = np.linalg.norm(x1 - self.center)
+
+    def find_elevation(self,lat,long):
+        # find the elevation of the lat,long point on the circle
+        # use the plane equation, double-check with sphere equation
+        denom = self.n[0] * np.cos(math.radians(lat)) * np.cos(math.radians(long)) \
+                + self.n[1] * np.cos(math.radians(lat)) * np.sin(math.radians(long)) \
+                + self.n[2] * np.sin(math.radians(lat))
+        elev = -self.d / denom - R_earth
+        new_point = lat_long_to_xyz(lat,long,elev)
+        new_r = np.linalg.norm(new_point - self.center)
+        return elev,new_point
+    
+    def tangent_line(self,x):
+        # find tangent to circle at point x
+        # assume x is actually on the circle
+        # use derivative of sphere and plane equation to calculate direction
+        delta = x - self.center
+        dzdx = ((self.n[0]/self.n[1]) * delta[1] - delta[0]) / (delta[2] - self.n[2]/self.n[1] * delta[1])
+        dydx = -self.n[2]/self.n[1] * dzdx - self.n[0]/self.n[1]
+        dir = np.array([1,dydx,dzdx])
+        return -dir/np.linalg.norm(dir)
+        
+
+def plot_tangent_line(circle,x,limit=10000,Lake_Crossings=None,label=None):
+    
+    fig, axs = plt.subplots(3,sharex=True)
+    fig.set_size_inches(8,10)
+    axs[2].set_xlabel('Distance from interaction Point [m]')
+    dir = circle.tangent_line(x)
+    trange = np.linspace(-limit,limit,2*1000)
+    points = x.reshape(-1,1) + np.outer(dir,trange)
+    FASER_envelope = np.abs(0.125/480*trange)
+    earth_points = np.array([xyz_to_lat_long(*p) for p in points.transpose()])
+    axs[0].plot(trange,earth_points[:,0],color='black')
+    axs[1].plot(trange,earth_points[:,1],color='black')
+    elev_range = earth_points[:,2] - Lake_geneva_elevation
+    axs[2].plot(trange,elev_range,color='black',label=label)
+    axs[2].fill_between(trange, elev_range - FASER_envelope, elev_range + FASER_envelope, color='black', alpha = 0.2, label = 'FASER envelope')
+    
+
+    # Plot lake crossings:
+    pairs  = [[Lake_Crossings[i],Lake_Crossings[i+1]] for i in range(0,len(Lake_Crossings),2)]
+    for i,pair in enumerate(pairs):
+        diffs = (np.linalg.norm(np.array(pair[0])[None,:] - earth_points[:,0:2],axis=1),
+                 np.linalg.norm(np.array(pair[1])[None,:] - earth_points[:,0:2],axis=1))
+        
+        t_intersects = (trange[np.argmin(diffs[0])],
+                        trange[np.argmin(diffs[1])])
+        axs[0].axvspan(t_intersects[0],t_intersects[1],color='blue',alpha=0.2)
+        axs[1].axvspan(t_intersects[0],t_intersects[1],color='blue',alpha=0.2)
+        X = np.linspace(t_intersects[0],t_intersects[1],100)
+        axs[2].fill_between(X,-1e5 * np.ones_like(X), np.zeros_like(X),color='blue',alpha=0.2,label='Lake Geneva' if i==0 else None)
+    
+    fig.subplots_adjust(hspace=0)
+    axs[2].legend()
+    axs[2].set_ylim(np.min(earth_points[:,2] - Lake_geneva_elevation),
+                    np.max(earth_points[:,2] - Lake_geneva_elevation))
+    axs[0].set_ylabel('Latitude [deg]')
+    axs[1].set_ylabel('Longitude [deg]')
+    axs[2].set_ylabel('Elevation w.r.t. Lake [m]')
+
+
+
+def lat_long_to_xyz(lat,long,elevation,
+                    lat_0 = 46.26,
+                    long_0 = 6.05):
+    R = R_earth + elevation
+    #r = Rotation.from_euler('zy',[long_0,lat_0],degrees=True)
+    point = np.array([
+        R * math.cos(math.radians(lat)) * math.cos(math.radians(long)),
+        R * math.cos(math.radians(lat)) * math.sin(math.radians(long)),
+        R * math.sin(math.radians(lat))]
+    )
+    return point#np.matmul(r.as_matrix(),point)
+
+def xyz_to_lat_long(x,y,z):
+    R = np.linalg.norm(np.array([x,y,z]))
+    lat = math.degrees(np.arcsin(z/R))
+    long = math.degrees(np.arctan2(y,x))
+    return lat,long,R-R_earth
+
+def equirectangular(lat,long,elevation,phi_0):
+    R = R_earth + elevation
+    return (
+        R * long * math.cos(math.radians(phi_0)),
+        R * lat,
+        elevation,
+    )
+
+# https://math.stackexchange.com/questions/1076177/3d-coordinates-of-circle-center-given-three-point-on-the-circle
+def find_circle_center(A,B,C):
+    u1 = B - A
+    w1 = np.cross((C - A),u1)
+    u = u1/np.linalg.norm(u1)
+    w = w1/np.linalg.norm(w1)
+    v = np.cross(w,u)
+    bx = np.dot(B-A,u)
+    cx = np.dot(C-A,u)
+    cy = np.dot(C-A,v)
+    h = ((cx - bx/2)**2 + cy**2 - (bx/2)**2)/(2*cy)
+    return A + (bx/2)*u + h*v
+          
+
+def plot_tangent_line_lat_long(circle,x,limit=10000,N=1000,**kwargs):
+    
+    dir = circle.tangent_line(x)
+    trange = np.linspace(-limit,limit,2*N+1)
+    points = x.reshape(-1,1) + np.outer(dir,trange)
+    earth_points = np.array([xyz_to_lat_long(*p) for p in points.transpose()])
+    return earth_points[N],earth_points
+    
+    
+def calculate_intersections_with_lake(circle,x,lake_coordinates,limit=50000):
+    point_lat_long = xyz_to_lat_long(*x)
+    lake_polygon = shapely.geometry.Polygon([[p[0],p[1]] for p in lake_coordinates])
+    dir = circle.tangent_line(x)
+    R = np.linalg.norm(x)
+    dR_dt = 1./R * np.dot(x,dir)
+    dlat_dt = 1./(R*np.sqrt(R**2 - x[2]**2)) * (R*dir[2] - x[2]*dR_dt)
+    dlong_dt = 1./(x[0]**2 + x[1]**2) * (-x[1]*dir[0] + x[0]*dir[1])
+    line1 = shapely.geometry.LineString([[point_lat_long[0], point_lat_long[1]],
+                                        [point_lat_long[0] + dlat_dt*limit, point_lat_long[1] + dlong_dt*limit]])
+    line2 = shapely.geometry.LineString([[point_lat_long[0], point_lat_long[1]],
+                                         [point_lat_long[0] - dlat_dt*limit, point_lat_long[1] - dlong_dt*limit]])
+    return line1.intersection(lake_polygon),line2.intersection(lake_polygon)
