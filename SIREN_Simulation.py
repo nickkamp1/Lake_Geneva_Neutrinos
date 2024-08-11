5c7498df5b77d6a0d18fa1dfa441dd654281b0cb
Nicholas Kamp <nkamp@fas.harvard.edu>
2024-08-11 15:40:00 -0400

    update the siren simulation to include the geneva detector and make some cluster scripts

--- SIREN_Simulation.py
+++ SIREN_Simulation.py
@@ -5,18 +5,17 @@ import argparse
 import numpy as np
 import awkward as ak
 
-def RunLHCbMuonSimulation(prefix,generator,parent,
+SIREN_dir = "/n/holylfs05/LABS/arguelles_delgado_lab/Everyone/nkamp/Geneva/Lake_Geneva_Neutrinos/Data/SIREN"
+
+def RunLHCbMuonSimulation(prefix,generator,parent,primary,
                           events_to_inject,outfile,
-                          lumi=3000):
-    
-    # Expeirment to run
-    experiment = "LakeGeneva"
+                          experiment="GemevaSurface",lumi=3000):
 
     # Define the controller
     controller = SIREN_Controller(events_to_inject, experiment)
 
     # Particle to inject
-    primary_type = siren.dataclasses.Particle.ParticleType.NuMu
+    primary_type = (siren.dataclasses.Particle.ParticleType)(primary)
 
     cross_section_model = "CSMSDISSplines"
 
@@ -31,17 +30,17 @@ def RunLHCbMuonSimulation(prefix,generator,parent,
         [primary_type],
         [target_type], "m"
     )
-    
+
     primary_xs = siren.interactions.InteractionCollection(primary_type, [DIS_xs])
     controller.SetInteractions(primary_xs)
 
-    
-    
+
+
     # Primary distributions
     primary_injection_distributions = {}
     primary_physical_distributions = {}
-    
-    siren_input_file = "Data/SIREN/Input/LHCb_%s_%s_%s.txt"%(prefix,generator,parent)
+
+    siren_input_file = "%s/Input/LHCb_%s_%s_%s_%d.txt"%(SIREN_dir,prefix,generator,parent,primary)
     assert(os.path.isfile(siren_input_file))
     with open(siren_input_file, "rbU") as f:
         num_input_events = sum(1 for _ in f) - 1
@@ -59,7 +58,7 @@ def RunLHCbMuonSimulation(prefix,generator,parent,
         primary_type, primary_injection_distributions, primary_physical_distributions
     )
 
-    
+
     # Run generation and save events
     controller.Initialize()
     controller.GenerateEvents()
@@ -67,89 +66,105 @@ def RunLHCbMuonSimulation(prefix,generator,parent,
                           hdf5=False, siren_events=False,
                           save_int_probs=True,
                           save_int_params=True)
-    
-    data = ak.from_parquet("%s.parquet"%outfile)
-    
-    mu_vertex = np.squeeze(data.vertex)
-    muon_momenta = np.array(np.squeeze(data.secondary_momenta[:,:,0]))
-    muon_momentum = np.array(np.linalg.norm(muon_momenta[:,1:],axis=1))
-    mu_dir = muon_momenta[:,1:] / np.expand_dims(muon_momentum,-1)
-    
-    panel_1 = controller.detector_model.GetSector("panel_1")
-    panel_2 = controller.detector_model.GetSector("panel_2")
-    panel_3 = controller.detector_model.GetSector("panel_3")
 
-    def GetPanelIntersections(location, direction):
-        _loc = siren.math.Vector3D(location)
-        _loc_detector = controller.detector_model.GeoPositionToDetPosition(siren.detector.GeometryPosition(_loc)).get()
-        _dir = siren.math.Vector3D(direction)
-        panel_intersections = {1:[],
-                               2:[],
-                               3:[]}
-        panel_distances = {1:[],
-                           2:[],
-                           3:[]}
-        panel_columndepths = {1:[],
-                              2:[],
-                              3:[]}
-        
-        for ip,panel in enumerate([panel_1,panel_2,panel_3]):
-            for intersection in panel.geo.Intersections(_loc,_dir):
-                panel_intersections[ip+1].append([intersection.position.GetX(),
-                                                  intersection.position.GetY(),
-                                                  intersection.position.GetZ()])
-                panel_distances[ip+1].append(intersection.distance)
-                int_loc_dectector = controller.detector_model.GeoPositionToDetPosition(siren.detector.GeometryPosition(intersection.position)).get()
-                panel_columndepths[ip+1].append(controller.detector_model.GetColumnDepthInCGS(_loc_detector,int_loc_dectector))
-        return panel_intersections,panel_distances,panel_columndepths
-    
-    panel_ints = {1:[],
-                  2:[],
-                  3:[]}
-    panel_dist = {1:[],
-                  2:[],
-                  3:[]}
-    panel_cdep = {1:[],
-                  2:[],
-                  3:[]}
-    hit_mask = {1:[],
-                2:[],
-                3:[]}
-    hit_mask_tot = []
-    for mv,md in zip(mu_vertex,mu_dir):
-        p_ints,p_dist,p_cdep = GetPanelIntersections(mv,md)
-        hit = False
-        for panel in p_ints.keys():
-            panel_ints[panel].append(p_ints[panel])
-            panel_dist[panel].append(p_dist[panel])
-            panel_cdep[panel].append(p_cdep[panel])
-            if sum(np.array(p_dist[panel])>0)>0:
-                hit = True
-                hit_mask[panel].append(True)
-            else: hit_mask[panel].append(False)
-            
-        hit_mask_tot.append(hit)
-    
-    for ik in panel_ints.keys():
-        data["panel%d_int_locations"%ik] = panel_ints[ik]
-        data["panel%d_int_distances"%ik] = panel_dist[ik]
-        data["panel%d_int_coldepths"%ik] = panel_cdep[ik]
-        data["panel%d_hit_mask"%ik] = hit_mask[ik]
-        
     weights = np.array(np.squeeze(data.wgt) * lumi * 1000 * np.prod(data.int_probs,axis=-1))
     weights *= num_input_events / events_to_inject # correct for sampled events
-    
     data["weights"] = weights
-    data["hit_mask"] = hit_mask_tot
-    
-    
-    ak.to_parquet(data,"%s.parquet"%outfile)
-    
+
+
+    # Muon intersection calculations
+    if experiment=="GenevaSurface":
+        data = ak.from_parquet("%s.parquet"%outfile)
+
+        mu_vertex = np.squeeze(data.vertex)
+        muon_momenta = np.array(np.squeeze(data.secondary_momenta[:,:,0]))
+        muon_momentum = np.array(np.linalg.norm(muon_momenta[:,1:],axis=1))
+        mu_dir = muon_momenta[:,1:] / np.expand_dims(muon_momentum,-1)
+
+        panel_1 = controller.detector_model.GetSector("panel_1")
+        panel_2 = controller.detector_model.GetSector("panel_2")
+        panel_3 = controller.detector_model.GetSector("panel_3")
+
+        def GetPanelIntersections(location, direction):
+            _loc = siren.math.Vector3D(location)
+            _loc_detector = controller.detector_model.GeoPositionToDetPosition(siren.detector.GeometryPosition(_loc)).get()
+            _dir = siren.math.Vector3D(direction)
+            panel_intersections = {1:[],
+                                2:[],
+                                3:[]}
+            panel_distances = {1:[],
+                            2:[],
+                            3:[]}
+            panel_columndepths = {1:[],
+                                2:[],
+                                3:[]}
+
+            for ip,panel in enumerate([panel_1,panel_2,panel_3]):
+                for intersection in panel.geo.Intersections(_loc,_dir):
+                    panel_intersections[ip+1].append([intersection.position.GetX(),
+                                                    intersection.position.GetY(),
+                                                    intersection.position.GetZ()])
+                    panel_distances[ip+1].append(intersection.distance)
+                    int_loc_dectector = controller.detector_model.GeoPositionToDetPosition(siren.detector.GeometryPosition(intersection.position)).get()
+                    panel_columndepths[ip+1].append(controller.detector_model.GetColumnDepthInCGS(_loc_detector,int_loc_dectector))
+            return panel_intersections,panel_distances,panel_columndepths
+
+        panel_ints = {1:[],
+                    2:[],
+                    3:[]}
+        panel_dist = {1:[],
+                    2:[],
+                    3:[]}
+        panel_cdep = {1:[],
+                    2:[],
+                    3:[]}
+        hit_mask = {1:[],
+                    2:[],
+                    3:[]}
+        hit_mask_tot = []
+        for mv,md in zip(mu_vertex,mu_dir):
+            p_ints,p_dist,p_cdep = GetPanelIntersections(mv,md)
+            hit = False
+            for panel in p_ints.keys():
+                panel_ints[panel].append(p_ints[panel])
+                panel_dist[panel].append(p_dist[panel])
+                panel_cdep[panel].append(p_cdep[panel])
+                if sum(np.array(p_dist[panel])>0)>0:
+                    hit = True
+                    hit_mask[panel].append(True)
+                else: hit_mask[panel].append(False)
+
+            hit_mask_tot.append(hit)
+
+        for ik in panel_ints.keys():
+            data["panel%d_int_locations"%ik] = panel_ints[ik]
+            data["panel%d_int_distances"%ik] = panel_dist[ik]
+            data["panel%d_int_coldepths"%ik] = panel_cdep[ik]
+            data["panel%d_hit_mask"%ik] = hit_mask[ik]
+
+        data["hit_mask"] = hit_mask_tot
+
+        muon_depth = siren.distributions.LeptonDepthFunction()
+
+        data["muon_max_col_depth"] = [muon_depth(siren.dataclasses.Particle.NuMu, muE)
+                                    for muE in muon_momenta[:,0]]
+
+        for panel in [1,2,3]:
+            data["panel%d_muon_survival"%panel] = data["panel%d_int_coldepths"%panel] < data["muon_max_col_depth"]
+            data["panel%d_hit_mask_muon_survival"%panel] = np.logical_and(data["panel%d_hit_mask"%panel],
+                                                                        np.any(data["panel%d_muon_survival"%panel],axis=-1))
+        data["hit_mask_muon_survival"] = np.logical_or.reduce((data["panel1_hit_mask_muon_survival"],
+                                                            data["panel2_hit_mask_muon_survival"],
+                                                            data["panel3_hit_mask_muon_survival"]))
+
+
+        ak.to_parquet(data[data["hit_mask"]==1],"%s.parquet"%outfile)
+
 def RunLHCbHNLSimulation(prefix,generator,parent,
                          events_to_inject,outfile,
                          m4="1000", Ue4=0, Umu4=1, Utau4=0,
                          lumi=3000):
-    
+
     # Expeirment to run
     experiment = "LakeGeneva"
 
@@ -159,11 +174,11 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
     # Particle to inject
     primary_type = siren.dataclasses.Particle.ParticleType.NuMu
     hnl_type = siren.dataclasses.Particle.ParticleType.N4
-    
+
     # Primary distributions
     primary_injection_distributions = {}
     primary_physical_distributions = {}
-    
+
     siren_input_file = "Data/SIREN/Input/LHCb_%s_%s_%s.txt"%(prefix,generator,parent)
     assert(os.path.isfile(siren_input_file))
     with open(siren_input_file, "rbU") as f:
@@ -176,7 +191,7 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
     fid_vol = controller.GetFiducialVolume()
     position_distribution = siren.distributions.PrimaryBoundedVertexDistribution(fid_vol)
     primary_injection_distributions["position"] = position_distribution
-    
+
     secondary_position_distribution = siren.distributions.SecondaryBoundedVertexDistribution(fid_vol)
 
     # SetProcesses
@@ -209,18 +224,18 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
     )
 
     DIS_interaction_collection = siren.interactions.InteractionCollection(primary_type, [DIS_xs])
-    
-    
+
+
     # Decay Model
     two_body_decay = siren.interactions.HNLTwoBodyDecay(float(m4)*1e-3, [Ue4, Umu4, Utau4], siren.interactions.HNLTwoBodyDecay.ChiralNature.Majorana)
     Decay_interaction_collection = siren.interactions.InteractionCollection(hnl_type, [two_body_decay])
-    
+
     controller.SetInteractions(primary_interaction_collection=DIS_interaction_collection,
                                secondary_interaction_collections=[Decay_interaction_collection])
-    
+
     # if we are below the W mass, use DarkNews for dimuon decay
     if float(m4)*1e-3 < siren.utilities.Constants.wMass:
-    
+
         # Define a DarkNews model
         model_kwargs = {
             "m4": float(m4)*1e-3,
@@ -247,35 +262,35 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
 
     ##################
 
-    
+
     # Run generation and save events
     controller.Initialize()
-    
+
     for process in controller.secondary_injection_processes:
         print(process.primary_type)
         for interaction in process.interactions.GetDecays():
             for signature in interaction.GetPossibleSignatures():
                 print(signature.secondary_types)
-                
+
     def stop(datum, i):
         secondary_type = datum.record.signature.secondary_types[i]
         return secondary_type != siren.dataclasses.Particle.ParticleType.N4
 
     controller.SetInjectorStoppingCondition(stop)
-    
+
     controller.GenerateEvents()
     controller.SaveEvents(outfile,
                           save_int_probs=True,
                           save_int_params=True)
-    
+
     # data = ak.from_parquet("%s.parquet"%outfile)
     # ak.to_parquet(data,"%s.parquet"%outfile)
-    
+
 #     mu_vertex = np.squeeze(data.vertex)
 #     muon_momenta = np.array(np.squeeze(data.secondary_momenta[:,:,0]))
 #     muon_momentum = np.array(np.linalg.norm(muon_momenta[:,1:],axis=1))
 #     mu_dir = muon_momenta[:,1:] / np.expand_dims(muon_momentum,-1)
-    
+
 #     panel_1 = controller.detector_model.GetSector("panel_1")
 #     panel_2 = controller.detector_model.GetSector("panel_2")
 #     panel_3 = controller.detector_model.GetSector("panel_3")
@@ -293,7 +308,7 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
 #         panel_columndepths = {1:[],
 #                               2:[],
 #                               3:[]}
-        
+
 #         for ip,panel in enumerate([panel_1,panel_2,panel_3]):
 #             for intersection in panel.geo.Intersections(_loc,_dir):
 #                 panel_intersections[ip+1].append([intersection.position.GetX(),
@@ -303,7 +318,7 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
 #                 int_loc_dectector = controller.detector_model.GeoPositionToDetPosition(siren.detector.GeometryPosition(intersection.position)).get()
 #                 panel_columndepths[ip+1].append(controller.detector_model.GetColumnDepthInCGS(_loc_detector,int_loc_dectector))
 #         return panel_intersections,panel_distances,panel_columndepths
-    
+
 #     panel_ints = {1:[],
 #                   2:[],
 #                   3:[]}
@@ -319,27 +334,35 @@ def RunLHCbHNLSimulation(prefix,generator,parent,
 #             panel_ints[panel].append(p_ints[panel])
 #             panel_dist[panel].append(p_dist[panel])
 #             panel_cdep[panel].append(p_cdep[panel])
-    
+
 #     for ik in panel_ints.keys():
 #         data["panel%d_int_locations"%ik] = panel_ints[ik]
 #         data["panel%d_int_distances"%ik] = panel_dist[ik]
 #         data["panel%d_int_coldepths"%ik] = panel_cdep[ik]
-        
+
 #     weights = np.array(np.squeeze(data.wgt) * lumi * 1000 * np.prod(data.int_probs,axis=-1))
 #     weights *= num_input_events / events_to_inject # correct for sampled events
-    
+
 #     data["weights"] = weights
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--prefix', type=str, help='forward-nu-flux prefix')
-    parser.add_argument('--generator', type=str, help='forward-nu-flux generator')
-    parser.add_argument('--parent', type=str, help='forward-nu-flux parent meson (light or charm)')
+    parser.add_argument('--case', type=str, help='simulation case (muon/hnl)', default='muon')
+    parser.add_argument('--primary', type=int, help='simulation primary PDG code', default=14)
+    parser.add_argument('-p','--prefix', type=str, help='forward-nu-flux prefix')
+    parser.add_argument('-g','--generator', type=str, help='forward-nu-flux generator')
+    parser.add_argument('-m','--parent', type=str, help='forward-nu-flux parent meson (light or charm)')
     parser.add_argument('-n', '--events-to-inject', type=int,help='number of events to inject')
     parser.add_argument('-o', '--output-file', type=str,help='output filename without extension')
-    # RunLHCbMuonSimulation(parser.prefix,parser.generator,parser.parent,
-    #                       parser.events_to_inject,args.output_file)
+    parser.add_argument('-e', '--experiment', type=str, default='GenevaSurface', help='experiment name (GenevaLake or GenevaSurface)')
+
     args = parser.parse_args()
-    RunLHCbHNLSimulation(args.prefix,args.generator,args.parent,
-                          args.events_to_inject,args.output_file)
+    if args.case=='muon':
+        RunLHCbMuonSimulation(args.prefix,args.generator,args.parent,args.primary,
+                              args.events_to_inject,args.output_file,args.experiment)
+    elif args.case=='hnl':
+        RunLHCbHNLSimulation(args.prefix,args.generator,args.parent,
+                             args.events_to_inject,args.output_file)
+    else:
+        print("Case %s not recognized"%args.case)
---
+++ cluster/SIREN/job_submission.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+
+light_generators = ["EPOSLHC","DPMJET",
+                    "SIBYLL","QGSJET","PYTHIA8"]
+charm_generators = ["BKSS","BKRS",
+                    "SIBYLL","BDGJKR","MS"]
+XSModel="CC"
+N = int(1e2)
+SIREN_dir = "/n/holylfs05/LABS/arguelles_delgado_lab/Everyone/nkamp/Geneva/Lake_Geneva_Neutrinos/Data/SIREN"
+experiment="GenevaLake"
+primaries = [12,-12,
+             14,-14,
+             16,-16]
+
+os.makedirs("%s/Output/%s"%(SIREN_dir,experiment),exist_ok=True)
+
+for primary in primaries:
+
+    if experiment=="GenevaSurface" and primary not in [14,-14]: continue
+
+    for gen_l,gen_c in zip(light_generators,charm_generators):
+
+        # light simulation
+        if primary not in [16,-16]:
+            output = "%s/Output/%s/LHCb_LHC13_%s_light_%s"%(SIREN_dir,experiment,gen_l,primary)
+            cmd = "sbatch --export=PRIMARY=%s,GEN=%s,MESON=light,N=%s,OUTPUT=%s,EXPERIMENT=%s submit_simulation.sbatch"%(primary,gen_l,N,output,experiment)
+            print(cmd)
+            os.system(cmd)
+
+        # charm simulation
+        output = "%s/Output/%s/LHCb_LHC13_%s_charm_%s"%(SIREN_dir,experiment,gen_c,primary)
+        cmd = "sbatch --export=PRIMARY=%s,GEN=%s,MESON=charm,N=%s,OUTPUT=%s,EXPERIMENT=%s submit_simulation.sbatch"%(primary,gen_c,N,output,experiment)
+        print(cmd)
+        os.system(cmd)
